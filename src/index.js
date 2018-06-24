1f0fb8430d7e1cf70094a1a9e063442a07c23896
niahoo <dev@ooha.in>
2018-06-25 01:19:44 +0700

    new prototype

--- src/index.js
+++ src/index.js
@@ -1,125 +1,244 @@
-function bindActionCreator(actionCreator, dispatch) {
-  return function () {
-    return dispatch(actionCreator.apply(this, arguments))
-  }
-}
+const __action_creators__ = Symbol('__action_creators__')
+const __is_plug__ = Symbol('__is_plug__')
+const __reducers__ = Symbol('__reducers__')
 
-export function composeReducers(...reducers) {
-  reducers = reducers.filter(red => red)
-  if (reducers.length === 0) {
-    return arg => arg
-  }
+// Create plug
+// - 1 We will create a new api of action creators with different
+//   properties of the plug:
+//   - actions are expected to return a payload. Reducers with the
+//     same name will reduce thoses actions. If the action returns a
+//     function, it will not be wrapped as a payload in order to let
+//     redux-thunk do its magic. If the payload has a string .type, a
+//     warning will be emitted but it will still be wrapped as a
+//     payload.
+//   - creators will be let unchanged.
+//   - mutations will be wrapped as action creators that return a
+//     callback reducer, and a reducer will be added to execute the
+//     callback.
 
-  if (reducers.length === 1) {
-    return reducers[0]
-  }
-
-  return (state, action) => reducers.reduce((state, f) => {
-    return f(state, action)
-  }, state)
-}
-
-
-export const createReducer = (initialState = {}) => {
-
-  const registry = {}
-
-  const reducer = (state = initialState, action) => {
-    const reducers = registry[action.type]
-    if (reducers) {
-      return composeReducers(...reducers)(state, action)
-    } else {
-      return state
+function createPlug(spec, opts) {
+    const scope = opts.scope ? `@@plug/${opts.scope}` : '@@plug'
+    const {
+        actions: actionSpecs,
+        creators: creatorSpecs,
+        mutations: mutationSpecs,
+    } = spec
+    let {
+        reducers: reducerSpecs,
+    } = spec
+    const plug = {
+        [__action_creators__]: [],
+        [__is_plug__]: true,
     }
-  }
-
-  reducer.register = registerFun => {
-    registerFun = typeof registerFun === 'function' ? registerFun
-         : typeof registerFun.register === 'function' ? registerFun.register
-         : null
-    if (null === registerFun) {
-      throw new Error("reducer.registerFun expects a function or an object with a register method.")
+    const reducers = reducersCollection( /*spec.reducers*/ )
+    if (typeof actionSpecs === 'object') {
+        Object.keys(actionSpecs).forEach(k => {
+            const actionType = `${scope}/${k}`
+            const creator = buildPayloadActionCreator(actionType, actionSpecs[k], plug)
+            plug[k] = creator
+            plug[__action_creators__].push(k)
+                // No reducer here, reducers for payload have to be
+                // declared and match the name
+        })
     }
-    // Call the reducer module function with a function that waits for action
-    // types and corresponding reducer
-    registerFun((actionTypes, reducer) => {
-      actionTypes = [].concat(actionTypes) // Handle a single action or array
-      actionTypes.forEach(type => {
-        if (! registry[type]) {
-          registry[type] = []
-        }
-        // unshift instead of push as we use composeReducers
-        registry[type].unshift(reducer)
-      })
-    })
-    return reducer // allows to chain plugs
-  }
-
-  reducer.plug = reducer.register // backward compat
-
-  return reducer
-}
-
-export const createPlugApp = function() {
-  const app = {}
-  const pluggableReducer = createReducer()
-
-  let dispatch = function() {
-    throw new Error("You must apply the store enhancer before adding plugs.")
-  }
-
-  const plug = function (scope, specs) {
-    if (arguments.length === 1) {
-      specs = scope
-      scope = '__root__'
+    if (typeof mutationSpecs === 'object') {
+        Object.keys(mutationSpecs).forEach(k => {
+            const actionType = `${scope}/${k}`
+            const creator = buildMutationActionCreator(actionType, mutationSpecs[k], plug)
+            plug[k] = creator
+            plug[__action_creators__].push(k)
+                // We will attach a default reducer for this mutation
+            reducers.add(actionType, applyClosureReducer)
+        })
     }
-    const { actions } = specs
-    const api = {}
-    if (typeof actions === 'object') {
-      Object.keys(actions).forEach(k => {
-        const actionType = `@@plug/${scope}/${k}`
-        const creator = buildActionCreator(actionType, actions[k])
-        const action = bindActionCreator(creator.creatorFunction, dispatch)
-        api[k] = action
-        const reducer = buildReducer(creator)
-        pluggableReducer.register(reduce => reduce(actionType, reducer))
-      })
+    if (typeof creatorSpecs === 'object') {
+        Object.keys(creatorSpecs).forEach(k => {
+            const creator = function actionCreator() {
+                return creatorSpecs[k].apply(plug, arguments)
+            }
+            plug[k] = creator
+            plug[__action_creators__].push(k)
+        })
     }
-    return scope === '__root__' ? api : {[scope]: api}
-  }
-
-  const enhancer = function(next) {
-    return function createStore(baseReducer, preloadedState, enhancer) {
-      const reducer = composeReducers(baseReducer, pluggableReducer)
-      const store = next(reducer, preloadedState, enhancer)
-      dispatch = store.dispatch
-      return store
+    // Base format for reducerSpecs :: reducerSpecs
+    //     reducerSpecs :: [reducerSpec]
+    //       reducerSpec :: [actionTypes, reducerFunctions]
+    //         actionTypes :: [String] | String
+    //         reducerFunctions :: [Function] | Function
+    // But we accept an object. In this case, the keys are prefixed
+    // with the scope
+    if (typeof reducerSpecs === 'object' && !Array.isArray(reducerSpecs)) {
+        const asArray = []
+        Object.keys(reducerSpecs).forEach(k => {
+            const actionType = `${scope}/${k}`
+            asArray.push([actionType, reducerSpecs[k]])
+        })
+        reducerSpecs = asArray
     }
-  }
-  app.enhancer = enhancer
-  app.plug = plug
-  return app
-}
-
-function buildActionCreator(actionType, spec) {
-  if (typeof spec === 'function') {
-    // If the actionCreator is a function, then it returns a reducer.
-    const creatorFunction = function actionCreator(...args) {
-      const reducer = spec(...args)
-      return { type: actionType, reducer }
+    if (Array.isArray(reducerSpecs)) {
+        reducerSpecs.forEach(reducerSpec => {
+            let [actionTypes, reducerFunctions] = reducerSpec
+            actionTypes = [].concat(actionTypes)
+            reducerFunctions = [].concat(reducerFunctions)
+            actionTypes.forEach(actionType => {
+                reducerFunctions.forEach(reducerFunction => {
+                    reducers.add(actionType, reducerFunction)
+                })
+            })
+        })
     }
-    return { creatorType: 'REDUCER_CLOSURE', creatorFunction}
-  }
-}
-
-function buildReducer(creator) {
-  const { creatorType } = creator
-  switch (creatorType) {
-    case 'REDUCER_CLOSURE':
-      return applyClosureReducer
-  }
+    plug[__reducers__] = reducers
+    return plug
 }
 
 function applyClosureReducer(state, { reducer }) {
-  return reducer(state)
+    return reducer(state)
+}
+
+function warnAboutPayloadTypeProperty(actionType) {
+    console.warn(`plug action '${actionType}' is wrapped in a payload but declares a 'type' property. You may want to declare a creator instead of an action`)
+}
+
+function buildPayloadActionCreator(actionType, userCreatorFn, self) {
+    const actionCreator = function() {
+        // here, self is the instance of action creators holders
+        const payload = userCreatorFn.apply(self, arguments)
+        if (typeof payload === 'function') {
+            // allow redux-thunk
+            return payload
+        }
+        if (payload && typeof payload.type === 'string') {
+            warnAboutPayloadTypeProperty(actionType)
+        }
+        return {
+            type: actionType,
+            payload
+        }
+    }
+    if (process.env.NODE_ENV === 'development') {
+        actionCreator._originalActionCreator = userCreatorFn
+    }
+    return actionCreator
+}
+
+function buildMutationActionCreator(actionType, userCreatorFn, self) {
+    const actionCreator = function() {
+        // here, self is the instance of action creators holders
+        const reducer = userCreatorFn.apply(self, arguments)
+        if (typeof reducer !== 'function') {
+            throw new Error(`Mutation ${actionType} must return a function.`)
+        }
+        return {
+            type: actionType,
+            reducer
+        }
+    }
+    if (process.env.NODE_ENV === 'development') {
+        actionCreator._originalActionCreator = userCreatorFn
+    }
+    return actionCreator
+}
+
+function reducersCollection( /*base*/ ) {
+    const registry = {}
+    return {
+        registry: () => registry,
+        add: (type, reducer) => {
+            const arr = (registry[type] = registry[type] || [])
+            arr.push(reducer)
+        },
+    }
+}
+
+export function composeReducers(...reducers) {
+    reducers = reducers.filter(red => red)
+    if (reducers.length === 0) {
+        return arg => arg
+    }
+
+    if (reducers.length === 1) {
+        return reducers[0]
+    }
+
+    return (state, action) => reducers.reduce((state, f) => {
+        return f(state, action)
+    }, state)
+}
+
+export const createReducer = () => {
+    const registry = {}
+    const reducer = (state = {}, action) => {
+        const reducers = registry[action.type]
+        if (reducers) {
+            return composeReducers(...reducers)(state, action)
+        } else {
+            return state
+        }
+    }
+    reducer.merge = function(collection) {
+        const patch = collection.registry()
+        Object.keys(patch).forEach(k => {
+            registry[k] = (registry[k] || []).concat(patch[k])
+        })
+    }
+    return reducer
+}
+
+export const createPlugApp = function() {
+    const pluggableReducer = createReducer()
+
+    let dispatch = function() {
+        throw new Error("You must apply the store enhancer before adding plugs.")
+    }
+
+    const pluggable = {}
+
+    Object.defineProperty(pluggable, 'plug', {
+        value: function attachPlug(scope, spec = {}, opts = {}) {
+            opts.scope = scope
+            const plug = createPlug(spec, opts)
+            pluggableReducer.merge(plug[__reducers__])
+            this[scope] = plug
+            return this
+        },
+        writable: false
+    })
+
+    Object.defineProperty(pluggable, 'getPlugApi', {
+        value: function attachPlug(scope) {
+            const plug = this[scope]
+            if (!plug || !plug[__is_plug__]) {
+                throw new Error(`Plug ${scope} does not exist.`)
+            }
+            const api = {}
+            plug[__action_creators__]
+                .forEach(k => {
+                    api[k] = bindPlugActionCreators(plug[k], dispatch, plug)
+                })
+            return api
+        },
+        writable: false
+    })
+
+    const enhancer = function(next) {
+        return function createStore(baseReducer, preloadedState, enhancer) {
+            const reducer = composeReducers(baseReducer, pluggableReducer)
+            const store = next(reducer, preloadedState, enhancer)
+            dispatch = store.dispatch
+            return store
+        }
+    }
+    Object.defineProperty(pluggable, 'enhancer', {
+        value: enhancer,
+        writable: false
+    })
+    return pluggable
+}
+
+function bindPlugActionCreators(actionCreator, dispatch, plug) {
+    // 'plug' is the actions creators holder instance created in
+    // attachPlug
+    return function() {
+        return dispatch(actionCreator.apply(plug, arguments))
+    }
 }
